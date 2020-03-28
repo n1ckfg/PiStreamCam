13820d431e1b1baebf83c9d6deaf0dccab57f28d
Nick Fox-Gieg <nick@fox-gieg.com>
2020-03-28 04:06:52 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ git clone https://github.com/n1ckfg/ofxIO
 git clone https://github.com/n1ckfg/ofxMediaType
 git clone https://github.com/n1ckfg/ofxNetworkUtils
 git clone https://github.com/n1ckfg/ofxSSLManager
+git clone https://github.com/n1ckfg/ofxCv
+git clone https://github.com/n1ckfg/ofxSSLManager
+git clone https://github.com/n1ckfg/ofxOMXCamera
