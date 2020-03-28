1bd6b64c5aad7b7550f176ec1ffe5b14a7ed3751
Nick Fox-Gieg <nick@fox-gieg.com>
2020-03-28 02:09:41 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+cd ../../../addons
+git clone https://github.com/n1ckfg/ofxHTTP
+git clone https://github.com/n1ckfg/ofxIO
+git clone https://github.com/n1ckfg/ofxMediaType
+git clone https://github.com/n1ckfg/ofxNetworkUtils
+git clone https://github.com/n1ckfg/ofxSSLManager
