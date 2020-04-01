c75db8f23fec24cc6959527700d0179fb3fcbe8e
Nick Fox-Gieg <nick@fox-gieg.com>
2020-04-01 17:42:13 -0400

    Update ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -79,8 +79,7 @@ void ofApp::setup() {
 void ofApp::update() {
     if (stillCam.isFrameNew()) {
         img.grabScreen(0, 0, width, height);
-        cout << stillCam.getPixels().getHeight() << endl;
-        server.send(stillCam.getPixels());
+        server.send(stillCam.getRawPixels());
     }
 }
 
