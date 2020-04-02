53f427f8c119657a8d92f03b0f02621b1e4ece27
Nick Fox-Gieg <nick@fox-gieg.com>
2020-04-02 13:43:37 -0400

    Update main.js

--- bin/data/DocumentRoot/js/main.js
+++ bin/data/DocumentRoot/js/main.js
@@ -5,22 +5,25 @@ var hostname="127.0.0.1";
 var port=7111;
 
 function main() {
-	loadFile("./js/hostname");
+	loadFile("./js/hostname", function(evt) {
+	    hostname = evt.replace(/^\n|\n$/g, ''); // remove line breaks
+        
+        liveView = document.getElementById("live_view");
+		liveView.src = "http://" + hostName + ".local:" + port;
+	});
 }
 
-function loadFile(file) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4) {
-            if (rawFile.status === 200 || rawFile.status == 0) {
-                hostname = rawFile.responseText;
-
-                liveView = document.getElementById("live_view");
-				liveView.src = "http://" + hostName + ".local:" + port;
-            }
+function loadFile(filepath, callback) { 
+    // https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript  
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("text/plain");
+    xobj.open('GET', filepath, true);
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            callback(xobj.responseText);
         }
-    }
+    };
+    xobj.send(null);  
 }
 
 window.onload = main;
