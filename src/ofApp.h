a6789d24cf86bf33edd5814893880cd9bb10a029
Nick Fox-Gieg <nick@fox-gieg.com>
2020-04-02 02:20:38 -0400

    Update ofApp.h

--- src/ofApp.h
+++ src/ofApp.h
@@ -41,4 +41,7 @@ class ofApp : public ofBaseApp, public ofxOMXPhotoGrabberListener {
 	        
 	    };
 
+	    void onHTTPPostEvent(ofxHTTP::PostEventArgs& evt);
+	    void onHTTPFormEvent(ofxHTTP::PostFormEventArgs& evt);
+	    void onHTTPUploadEvent(ofxHTTP::PostUploadEventArgs& evt);
 };
