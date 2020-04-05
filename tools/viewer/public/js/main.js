47f0e07138e570b568f2b9a3679c9fc95a568a4e
Nick Fox-Gieg <nick@fox-gieg.com>
2020-04-05 14:02:10 -0400

    Update main.js

--- tools/viewer/public/js/main.js
+++ tools/viewer/public/js/main.js
@@ -1,16 +1,20 @@
 "use strict";
 
-var camNameBegin = "nfg-rpi-";
+var camNameBegin = "ws://nfg-rpi-";
 var camNameList = [ "0-1", "2-1", "2-2", "3-1", "3-2", "3-3", "3-4" ];
-var camNameEnd = ".local:7110";
-var activeCams = [];
+var camNameEnd = ".local:7112";
+var camUrls = [];
+var camWs = [];
 
 function main() {
-	//loadPages();
+	makeCamUrls();
+	openCamConnections();
 	setupKeys();
 }
 
 window.onload = main;
+
+// ~ ~ ~ ~ ~ ~ ~ ~ 
 
 function setupKeys() {
 	window.addEventListener("keydown", function(event) {
@@ -28,39 +32,62 @@ function getKeyCode(event) {
     return c;
 }
 
-// https://stackoverflow.com/questions/5211328/clicking-submit-button-of-an-html-form-by-a-javascript-code
 function takePhoto() {
 	console.log("Taking photos...");
-	for (var i=0; i<activeCams.length; i++) {
-		//
+	for (var i=0; i<camWs.length; i++) {
+		camWs[i].send("Hello");
 	}
 }
 
-// https://gomakethings.com/getting-html-asynchronously-from-another-page/
-function getHtml(url, callback) {
-	if (!window.XMLHttpRequest) return; // Feature detection
-
-	var xhr = new XMLHttpRequest();
-
-	xhr.onload = function() {
-		if (callback && typeof(callback) === 'function' ) {
-			callback(this.responseXML);
-		}
-	}
-
-	xhr.open( 'GET', url );
-	xhr.responseType = 'document';
-	xhr.send();
-}
-
-function loadPages() {
+function makeCamUrls() {
 	for (var i=0; i<camNameList.length; i++) {
-		var camUrl = "http://" + camNameBegin + camNameList[i] + camNameEnd;
-		console.log(camUrl);
+		var camUrl = camNameBegin + camNameList[i] + camNameEnd;
 
-		var newDoc = getHtml(camUrl, function(evt) {
-			console.log(evt);
-		});
+		camUrls.push(camUrl);
 	}
 }
 
+function openCamConnections() {
+	for (var i=0; i<camUrls.length; i++) {
+		console.log("Attempting to open " + camUrls[i]);
+		try {
+			var ws = new WebSocket(camUrls[i]);
+			camWs.push(ws);
+		} catch (e) { }
+	}	
+}
+
+// ~ ~ ~ ~ ~ ~ ~ ~ 
+
+var wsUrl = "ws://nfg-rpi-3-4.local:7112";
+
+function setupWs() {
+	websocket = new WebSocket(wsUrl);
+	websocket.onopen = function(evt) { onOpen(evt) };
+	websocket.onclose = function(evt) { onClose(evt) };
+	websocket.onmessage = function(evt) { onMessage(evt) };
+	websocket.onerror = function(evt) { onError(evt) };
+}
+
+function onOpen(evt) {
+	console.log("CONNECTED");
+	doSend("Hello");
+}
+
+function onClose(evt) {
+	console.log("DISCONNECTED");
+}
+
+function onMessage(evt) {
+	console.log("RESPONSE: " + evt.data);
+	websocket.close();
+}
+
+function onError(evt) {
+	console.log("ERROR: " + evt.data);
+}
+
+function doSend(message) {
+	console.log("SENT: " + message);
+	websocket.send(message);
+}
