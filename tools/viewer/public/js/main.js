4d0748e9cefd168b7320d97ee88f54bfa31ee945
Nick Fox-Gieg <nick@fox-gieg.com>
2020-04-06 01:02:06 -0400

    htaccess

--- tools/viewer/public/js/main.js
+++ tools/viewer/public/js/main.js
@@ -76,12 +76,73 @@ function onMessage(evt) {
 		if (results[0] == camNameList[i]) {
 			var url = "http://" + camNameList[i] + ".local:7110/photos/" + results[1];
 			console.log("RESPONSE: " + url);
+			
+			loadToCanvas(url);
+
 			stillBoxes[i].style.backgroundImage = "url(\"" + url + "\")";
-			stillBoxes[i].style.backgroundSize = "100px 75px";
+			stillBoxes[i].style.backgroundSize = "100px 75px";	
+
 			break;
 		}
 	}
 }
+
+function loadToCanvas(url) {
+	var img = new Image;
+	var c = document.createElement("canvas");
+	var ctx = c.getContext("2d");
+
+	img.onload = function() {
+	  c.width = this.naturalWidth;     // update canvas size to match image
+	  c.height = this.naturalHeight;
+	  ctx.drawImage(this, 0, 0);       // draw in image
+	  c.toBlob(function(blob) {        // get content as JPEG blob
+	    // here the image is a blob
+	    downloadBlob(blob, "test.jpg");
+	  }, "image/jpeg", 1.0);
+	};
+	img.crossOrigin = "Anonymous";              // if from different origin
+	img.src = url;
+}
+
+function downloadBlob(blob, filename) {
+  // Create an object URL for the blob object
+  const url = URL.createObjectURL(blob);
+  
+  // Create a new anchor element
+  const a = document.createElement('a');
+  
+  // Set the href and download attributes for the anchor element
+  // You can optionally set other attributes like `title`, etc
+  // Especially, if the anchor element will be attached to the DOM
+  a.href = url;
+  a.download = filename || 'download';
+  
+  // Click handler that releases the object URL after the element has been clicked
+  // This is required for one-off downloads of the blob content
+  const clickHandler = () => {
+    setTimeout(() => {
+      URL.revokeObjectURL(url);
+      this.removeEventListener('click', clickHandler);
+    }, 150);
+  };
+  
+  // Add the click event listener on the anchor element
+  // Comment out this line if you don't want a one-off download of the blob content
+  a.addEventListener('click', clickHandler, false);
+  
+  // Programmatically trigger a click on the anchor element
+  // Useful if you want the download to happen automatically
+  // Without attaching the anchor element to the DOM
+  // Comment out this line if you don't want an automatic download of the blob content
+  a.click();
+  
+  // Return the anchor element
+  // Useful if you want a reference to the element
+  // in order to attach it to the DOM or use it in some other way
+  return a;
+}
+
 
 /*
 var wsUrl = "ws://nfg-rpi-3-4.local:7112";
