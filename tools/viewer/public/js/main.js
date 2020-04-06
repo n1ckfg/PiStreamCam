61f7991d3dfab2790b44ecfc0ab25c79e025b1c9
Nick Fox-Gieg <nick@fox-gieg.com>
2020-04-06 01:30:59 -0400

    Update main.js

--- tools/viewer/public/js/main.js
+++ tools/viewer/public/js/main.js
@@ -77,7 +77,8 @@ function onMessage(evt) {
 			var url = "http://" + camNameList[i] + ".local:7110/photos/" + results[1];
 			console.log("RESPONSE: " + url);
 			
-			loadToCanvas(url);
+			var filename = url.split("/")[url.split("/").length-1];
+			download(filename, url);
 
 			stillBoxes[i].style.backgroundImage = "url(\"" + url + "\")";
 			stillBoxes[i].style.backgroundSize = "100px 75px";	
@@ -87,60 +88,18 @@ function onMessage(evt) {
 	}
 }
 
-function loadToCanvas(url) {
-	var img = new Image;
-	var c = document.createElement("canvas");
-	var ctx = c.getContext("2d");
+function download(filename, url) {
+    var element = document.createElement('a');
+    //element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('href', url);
+    element.setAttribute('download', filename);
+    element.setAttribute('target', '_blank');
+    element.style.display = 'none';
+    document.body.appendChild(element);
 
-	img.onload = function() {
-	  c.width = this.naturalWidth;     // update canvas size to match image
-	  c.height = this.naturalHeight;
-	  ctx.drawImage(this, 0, 0);       // draw in image
-	  c.toBlob(function(blob) {        // get content as JPEG blob
-	    // here the image is a blob
-	    downloadBlob(blob, "test.jpg");
-	  }, "image/jpeg", 1.0);
-	};
-	img.crossOrigin = "";              // if from different origin
-	img.src = url;
-}
+    element.click();
 
-function downloadBlob(blob, filename) {
-  // Create an object URL for the blob object
-  const url = URL.createObjectURL(blob);
-  
-  // Create a new anchor element
-  const a = document.createElement('a');
-  
-  // Set the href and download attributes for the anchor element
-  // You can optionally set other attributes like `title`, etc
-  // Especially, if the anchor element will be attached to the DOM
-  a.href = url;
-  a.download = filename || 'download';
-  
-  // Click handler that releases the object URL after the element has been clicked
-  // This is required for one-off downloads of the blob content
-  const clickHandler = () => {
-    setTimeout(() => {
-      URL.revokeObjectURL(url);
-      this.removeEventListener('click', clickHandler);
-    }, 150);
-  };
-  
-  // Add the click event listener on the anchor element
-  // Comment out this line if you don't want a one-off download of the blob content
-  a.addEventListener('click', clickHandler, false);
-  
-  // Programmatically trigger a click on the anchor element
-  // Useful if you want the download to happen automatically
-  // Without attaching the anchor element to the DOM
-  // Comment out this line if you don't want an automatic download of the blob content
-  a.click();
-  
-  // Return the anchor element
-  // Useful if you want a reference to the element
-  // in order to attach it to the DOM or use it in some other way
-  return a;
+    document.body.removeChild(element);
 }
 
 
